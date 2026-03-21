cc39821780f75595523f4e3dc31c521ed0929c11
Jairo <jaiiro.cereceda@gmail.com>
2026-03-21 23:20:09 +0100

    fix: PWA for ios

--- app/+html.tsx
+++ app/+html.tsx
@@ -12,6 +12,10 @@ export default function HTML({ children }: PropsWithChildren) {
         />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#F6F1EB" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="SushiCount" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <title>SushiCount</title>
 
         <script
