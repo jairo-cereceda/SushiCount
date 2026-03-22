5b2bf3ebfd17f1f5bb6247451ce592b9fff7f1ef
Jairo <jaiiro.cereceda@gmail.com>
2026-03-22 20:28:16 +0100

    fix: PWA title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,24 @@ import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import Head from "expo-router/head";
 
 import { colors } from "@/styles/tokens";
 
 export default function RootLayout() {
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false, title: "SushiCount" }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="dark" backgroundColor={colors.background} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>SushiCount</title>
+        <meta name="description" content="Contador de sushi" />
+      </Head>
+
+      <ThemeProvider value={DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false, title: "SushiCount" }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+        <StatusBar style="dark" backgroundColor={colors.background} />
+      </ThemeProvider>
+    </>
   );
 }
